6c79419ee8811480cd8ea9be1a0ef23095da2b96
Jonathan Masters <jonathan_masters77@yahoo.co.uk>
2014-10-01 16:22:15 +0100

    Traffic and Travel Buses Template started

--- src/static/js/travelapp.js
+++ src/static/js/travelapp.js
@@ -4,3 +4,26 @@
 
 var tmTravelApp = tmTravelApp || {};
 
+/**
+ * Open Order Menu
+ */
+
+$(document).ready(function() {
+    $("#order-menu-btn").click(function(event) {
+        $(this).find('span').toggleClass('icon-order-active');
+        $(".refresh-menu").toggleClass('refresh-menu-active');
+    });
+
+});
+
+/**
+ * Roads Order Menu JS
+ */
+
+$(document).ready(function() {
+    $(".order-items li a ").click(function(event) {
+        $(".order-items li a span").attr('class', 'order-icon');
+        $(this).find('span').addClass('order-active');
+    });
+
+});
